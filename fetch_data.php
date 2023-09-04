9c850d8893eb4f082d6f6158eba8ddd88735bf64
unknown <j.manzur02@gmail.com>
2023-09-03 21:16:19 -0500

    Hecho

--- fetch_data.php
+++ fetch_data.php
@@ -1,8 +1,8 @@
 <?php
 // Database connection
-$dbHost = 'localhost';
-$dbUser = 'root';
-$dbPass = 'O@rlando0310';
+$dbHost = 'basededatos.cnbw6moot2c2.us-east-1.rds.amazonaws.com';
+$dbUser = 'manzur';
+$dbPass = 'JuanD0212_2003';
 $dbName = 'basededatos';
 
 $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="script.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAp5birBy8ELAwJKpgqApyKXgQkPzUFjAg&callback=initMap"></script>
-    <title>Real-Time GPS Data</title>
+    <title>Location on Map</title>
+    <style>
+        #map {
+            height: 400px;
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
@@ -14,7 +17,11 @@
         <div class="data-container" id="dataContainer">
             <!-- Data will be dynamically inserted here -->
         </div>
-        <div id="map" style="width: 100%; height: 500px;"></div>
+        <div id="map"></div>
     </div>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAp5birBy8ELAwJKpgqApyKXgQkPzUFjAg"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="script.js"></script>
 </body>
 </html>

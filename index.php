a870683d68a241cf32acd83514a6457cbb9b6ce5
unknown <j.manzur02@gmail.com>
2023-09-16 16:13:58 -0500

    Changes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
             width: 100%;
         }
     </style>
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.min.css">
 </head>
 <body>
     <div class="container">
@@ -24,12 +24,13 @@
         <label for="fechaFinal">Fecha Final:</label>
         <input type="text" id="fechaFinal" name="fechaFinal">
         <div id="map"></div>
+        <button id="borrar-rutas">Borrar rutas</button>
+
+
     </div>
 
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAp5birBy8ELAwJKpgqApyKXgQkPzUFjAg"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-   
     <script src="script.js"></script>
 </body>
 </html>

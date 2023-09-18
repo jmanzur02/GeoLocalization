bfd98a73447124dad52eebab0b19f77fe2d5430d
unknown <j.manzur02@gmail.com>
2023-09-18 18:08:27 -0500

    Changes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
             width: 100%;
         }
     </style>
-    
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.min.css">
 </head>
 <body>
     <div class="container">
@@ -31,15 +31,12 @@
         <button id="trazar-ruta">Trazar ruta</button>
         <div id="map"></div>
         <button id="borrar-rutas">Borrar rutas</button>
-
-
     </div>
 
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAp5birBy8ELAwJKpgqApyKXgQkPzUFjAg"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAp5birBy8ELAwJKpgqApyKXgQkPzUFjAg"></script>
     <script src="https://cdn.rawgit.com/xdan/datetimepicker/gh-pages/jquery.datetimepicker.full.min.js"></script>
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.min.css">
     <script src="script.js"></script>
 </body>
 </html>

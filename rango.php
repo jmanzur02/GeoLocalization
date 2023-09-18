4e552210225329cb1d322e4c5f2706946145f29c
unknown <j.manzur02@gmail.com>
2023-09-18 16:29:47 -0500

    Changes

--- rango.php
+++ rango.php
@@ -5,38 +5,47 @@ $dbUser = 'manzur';
 $dbPass = 'JuanD0212_2003';
 $dbName = 'basededatos';
 
-
+// Validate and sanitize input
 $fechaInicio = $_POST['fechaInicio'];
 $fechaFinal = $_POST['fechaFinal'];
 
+// Validate date format (adjust this format if needed)
+if (!strtotime($fechaInicio) || !strtotime($fechaFinal)) {
+    die("Invalid date format provided.");
+}
 
 $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$sqlRango = "SELECT Latitud, Longitud FROM posicion WHERE Timestamp >= ? AND Timestamp <= ?";
+$stmt = $conn->prepare($sqlRango);
 
+// Bind parameters
+$stmt->bind_param("ss", $fechaInicio, $fechaFinal);
 
+// Execute the query
+if ($stmt->execute()) {
+    $resultRango = $stmt->get_result();
 
-$sqlRango = "SELECT Latitud, Longitud FROM posicion WHERE Timestamp >= '$fechaInicio' AND Timestamp <= '$fechaFinal'";
-$resultRango = $conn->query($sqlRango);
-
-$dataRango = [];
-if ($resultRango->num_rows > 0) {
+    $dataRango = [];
     while ($row = $resultRango->fetch_assoc()) {
         $dataRango[] = [
             'Latitud' => $row['Latitud'],
             'Longitud' => $row['Longitud']
         ];
     }
+
+    // Close the database connection
+    $stmt->close();
+    $conn->close();
+
+    // Return data as JSON
+    header('Content-Type: application/json');
+    echo json_encode($dataRango);
+} else {
+    // Handle query execution error
+    echo "Error executing the query: " . $stmt->error;
 }
-
-// Close the database connection
-$conn->close();
-
-
-
-// Return data as JSON
-header('Content-Type: application/json');
-echo json_encode($dataRango);
 ?>

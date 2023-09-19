48aac6a362b928ff1a0e11809c2723b5b732a87a
unknown <j.manzur02@gmail.com>
2023-09-18 19:41:13 -0500

    Changes

--- rango.php
+++ rango.php
@@ -7,14 +7,9 @@ $dbName = 'basededatos';
 
 // Obtén las fechas de inicio y finalización desde el cliente
 $fechaInicio = $_GET['fechaInicio'];
-var_dump($fechaInicio);
 $fechaFinal = $_GET['fechaFinal'];
-var_dump($fechaFinal);
 
-// Validación de formato de fecha (ajusta el formato según tus datos)
-if (!strtotime($fechaInicio) || !strtotime($fechaFinal)) {
-    die("Formato de fecha inválido. Asegúrate de que esté en el formato correcto.");
-}
+
 
 $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
 if ($conn->connect_error) {

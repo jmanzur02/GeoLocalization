b487c1daedc6f41bdce7a9640aea8accd06bdadc
unknown <j.manzur02@gmail.com>
2023-09-24 12:59:41 -0500

    changes

--- script.js
+++ script.js
@@ -39,22 +39,27 @@ $(document).ready(function() {
 
 
         $.ajax({
-                type: "GET", 
-                url: "latlong.php",
-                dataType: "json",
-                data: {
-                    latitud: latitud,
-                    longitud: longitud,
-                    fechaInicio: fechaInicio,
-                    fechaFinal: fechaFinal,
-                    horaInicial: horaInicial,
-                    horaFinal: horaFinal
-                },
-                success: function(latlong) {
-                    // Procesa los datos y traza la ruta
-                    console.log("Los parámetros recibidos son: " + JSON.stringify(latlong));
-                },
-            });
+            type: "GET", 
+            url: "latlong.php",
+            dataType: "json",
+            data: {
+                latitud: latitud,
+                longitud: longitud,
+                fechaInicio: fechaInicio,
+                fechaFinal: fechaFinal,
+                horaInicial: horaInicial,
+                horaFinal: horaFinal
+            },
+            success: function(latlong) {
+                // Procesa los datos y traza la ruta
+                console.log("Los parámetros recibidos son: " + JSON.stringify(latlong));
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                // Esta función se ejecutará si ocurre un error en la solicitud AJAX
+                console.log("Error en la solicitud AJAX: " + textStatus + " - " + errorThrown);
+                // Puedes mostrar un mensaje de error o realizar otras acciones de manejo de errores aquí
+            }
+        });
         
         // Aquí puedes realizar otras acciones con las coordenadas, como enviarlas al servidor o mostrarlas en la página.
     });
